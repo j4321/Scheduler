7dfc545338cab656302ae31e99489779205d4b81
Juliette Monsel <j_4321@protonmail.com>
2019-01-02 14:58:23 +0100

    Fix copyrights

--- schedulerlib/settings/__init__.py
+++ schedulerlib/settings/__init__.py
@@ -1,1 +1,23 @@
+#! /usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+Scheduler - Task scheduling and calendar
+Copyright 2017-2018 Juliette Monsel <j_4321@protonmail.com>
+
+Scheduler is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Scheduler is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+printGNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+Settings module
+"""
 from .settings import Settings
