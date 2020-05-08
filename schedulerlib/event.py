6a4caea8b7007dbdf2c0c048d02c031c575beb52
Juliette Monsel <j_4321@protonmail.com>
2020-05-08 19:47:51 +0200

    Add possibility to load .ics data from url

--- schedulerlib/event.py
+++ schedulerlib/event.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """
 Scheduler - Task scheduling and calendar
-Copyright 2017-2019 Juliette Monsel <j_4321@protonmail.com>
+Copyright 2017-2020 Juliette Monsel <j_4321@protonmail.com>
 
 Scheduler is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -143,7 +143,6 @@ class Event:
                 if action and action != "NONE":
                     dt = props['Start'] + component.get("trigger").dt
                     ev.reminder_add(dt)
-        print(ev.to_dict())
         return ev
 
     def __str__(self):
